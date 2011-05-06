2b267f3f6377ae631cc53ecc15762123f9aa050c
debug <debug@d3684b96-7d84-fda5-3fc2-c2da22a16284>
2011-05-06 11:51:51 +0000

    testing rand characters and random plots in character grid

--- mymatrix.c
+++ mymatrix.c
@@ -8,20 +8,20 @@ int maxy, maxx;
 srand(time(NULL));
 initscr();
 getmaxyx(stdscr, maxy, maxx);
-int matrix[maxy][4];
+short int matrix[maxy][4];
 int x;
-char randchr(){
-return (rand()&127/(127/96))+31;
+unsigned char randchr(){
+return (unsigned char)rand()*96/256+31;
 srand(rand());
 }
 for(x=0;x<=maxy;++x){
-matrix[x][1] = - (unsigned short int)rand()/(65536/maxy);
+matrix[x][1] = (short int)-((unsigned short int)rand()*maxy/65536);
 srand(rand());
 //waddch(stdscr,(char)matrix[x][1]);
 //refresh();
 }
 for(x=0;x<=maxx;++x){
-matrix[x][2] = (unsigned short int)rand()/(65536/maxx);
+matrix[x][2] = (unsigned short int)rand()*maxx/65536;
 srand(rand());
 }
 // mvwaddstr(stdscr, x, y, "text"); print single string at x, y
@@ -33,7 +33,7 @@ srand(rand());
 // mvwprintw(stdscr, y, x, "printf string", var); printf equiv for ncurses
 // curs_set(0);
 // sleep(3); can allow some time to enjoi view
-mvaddch((rand()&65535)/(65535/maxy),(rand()&65535)/(65535/maxx),randchr());
+mvaddch(rand()*maxy/65536,rand()*maxx/65536,randchr());
 refresh();
 sleep(2);
 
--- test.c
+++ test.c
@@ -4,8 +4,9 @@
 int main() {
 srand(time(NULL));
 int x;
-for(x=0;x<=100;++x){
-printf("%c",(char)((rand()&127/(127/96))+31));
+for(x=0;x<=255;++x){
+//printf("%c",(char)((rand()&127/(127/96))+31));
+printf("%d\n",(unsigned short int)rand()*100/65536);
 srand(rand());
 }
 printf("\n");
