4a31d2fbccde93c41f076bd1f58c287cff5ce16b
debug <debug@d3684b96-7d84-fda5-3fc2-c2da22a16284>
2011-05-06 14:18:26 +0000

    matrix init verified, need to start on clear row as well as print row

--- mymatrix.c
+++ mymatrix.c
@@ -5,25 +5,40 @@
 
 int main() {
 int maxy, maxx;
+int x;
 srand(time(NULL));
 initscr();
 getmaxyx(stdscr, maxy, maxx);
-short int matrix[maxy][4];
-int x;
+int lines=.9*maxx;
+short int matrix[lines][3];
 unsigned char randchr(){
+srand(rand());
 return (unsigned char)rand()*96/256+31;
-srand(rand());
 }
-for(x=0;x<=maxy;++x){
-matrix[x][1] = (short int)-((unsigned short int)rand()*maxy/65536);
+//init matrix data
+for(x=1;x<=lines;++x){
 srand(rand());
-//waddch(stdscr,(char)matrix[x][1]);
-//refresh();
-}
-for(x=0;x<=maxx;++x){
-matrix[x][2] = (unsigned short int)rand()*maxx/65536;
+matrix[x][1] = (unsigned short int)rand()*maxy/65536;
 srand(rand());
+matrix[x][2] = (unsigned short int)rand()*maxy/65536;
+if(matrix[x][2]>=matrix[x][1]){
+matrix[x][2] = matrix[x][2] - maxy;
 }
+srand(rand());
+matrix[x][3] = (unsigned short int)rand()*maxx/65536;
+}
+// refresh layer
+for(x=1;x<=lines;++x){
+//mvwprintw(stdscr,0,0,"%d",matrix[x][1]);
+//mvwprintw(stdscr,1,0,"%d",matrix[x][2]);
+mvwprintw(stdscr,matrix[x][1],matrix[x][3],"%c",randchr());
+//mvwprintw(stdscr,(unsigned short int)rand()*maxy/65536,(unsigned short int)rand()*maxx/65536,"%c",randchr());
+refresh();
+}
+sleep(2);
+endwin();
+
+return 0;
 // mvwaddstr(stdscr, x, y, "text"); print single string at x, y
 // mvaddch(y,x,c); move cursor to x, y
 // waddch(stdscr, c); write char c at current cursor position and shift right one
@@ -33,13 +48,4 @@ srand(rand());
 // mvwprintw(stdscr, y, x, "printf string", var); printf equiv for ncurses
 // curs_set(0);
 // sleep(3); can allow some time to enjoi view
-for(;;){
-mvwprintw(stdscr,(unsigned short int)rand()*maxy/65536,(unsigned short int)rand()*maxx/65536,"%c",randchr());
-refresh();
-}
-sleep(2);
-
-endwin();
-
-return 0;
 }
--- test.c
+++ test.c
@@ -3,11 +3,15 @@
 
 int main() {
 srand(time(NULL));
+unsigned short int matrix[100][1];
 int x;
-for(x=0;x<=255;++x){
-//printf("%c",(char)((rand()&127/(127/96))+31));
-printf("%d\n",(unsigned short int)rand()*100/65536);
+for(x=1;x<=100;++x){
 srand(rand());
+matrix[x][1] = (unsigned short int)rand()*100/65536;
+}
+for(x=1;x<=100;++x){
+//printf("%c",(char)((rand()&127/(127/96))+31));
+printf("%d\n",matrix[x][1]);
 }
 printf("\n");
 }
