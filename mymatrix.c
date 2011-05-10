6e825ff9b3ccf99f0eb338903b0bfa6800340aeb
debug <debug@d3684b96-7d84-fda5-3fc2-c2da22a16284>
2011-05-10 01:03:45 +0000

    removed all srand functions except initialization and added usleep to increase delay since it ran so fast

--- mymatrix.c
+++ mymatrix.c
@@ -20,18 +20,13 @@ getmaxyx(stdscr, maxy, maxx);
 unsigned int lines=1.5*maxx;
 short int matrix[lines][5];
 unsigned char randchr(){
-srand(rand());
 return (unsigned char)rand()*95/256+32;
 }
 for(x=1;x<=lines;++x){
-srand(rand());
 matrix[x][1] = -(unsigned short int)rand()*maxy/65536;
-srand(rand());
 matrix[x][2] = -(unsigned short int)rand()*maxy/65536;
 if(matrix[x][2]>matrix[x][1]){matrix[x][2]=matrix[x][2]-(unsigned short int)rand()*maxy/65536;}
-srand(rand());
 matrix[x][3] = (unsigned short int)rand()*maxx/65536;
-srand(rand());
 matrix[x][5] = rand();
 }
 while(1){
@@ -43,7 +38,6 @@ attron(COLOR_PAIR(1));
 if(matrix[x][1]>=0){
 mvprintw(matrix[x][1],matrix[x][3],"%c",matrix[x][4]);
 }
-srand(rand());
 if(matrix[x][5]>(short int)rand()){
 matrix[x][1] = matrix[x][1]+1;
 matrix[x][4] = randchr();
@@ -52,17 +46,13 @@ attron(COLOR_PAIR(2));
 if(matrix[x][1]>=0){
 mvprintw(matrix[x][1],matrix[x][3],"%c",matrix[x][4]);
 }
-srand(rand());
 if(matrix[x][5]>(short int)rand()){
 matrix[x][2] = matrix[x][2]+1;
 if(matrix[x][2]>matrix[x][1]){matrix[x][2]=matrix[x][2]-(unsigned short int)rand()*maxy/65536;}
 if(matrix[x][2]>maxy){
 matrix[x][1]=0;
-srand(rand());
 matrix[x][2]=-(unsigned short int)rand()*maxy/65536;
-srand(rand());
 matrix[x][3]=(unsigned short int)rand()*maxx/65536;
-srand(rand());
 matrix[x][5]=rand();
 }
 }
@@ -72,12 +62,12 @@ mvprintw(matrix[x][2],matrix[x][3]," ");
 }
 attron(COLOR_PAIR(1));
 for(x=0;x<=10;++x){
-srand(rand());
 move((unsigned short int)rand()*maxy/65536,(unsigned short int)rand()*maxx/65536);
 if((char)(A_CHARTEXT&inch())!=' '){
 printw("%c",randchr());
 }
 refresh();
+usleep(800);
 }
 }
 }
