7d8d6a82fbfcbca1720c015404388dac0d71f849
debug <debug@d3684b96-7d84-fda5-3fc2-c2da22a16284>
2012-07-30 23:02:05 +0000

    finally added comments and cleaned up the code just a tiny bit

--- rmatrix.c
+++ rmatrix.c
@@ -3,9 +3,12 @@
 #include <stdlib.h>
 
 int main() {
+/* variables */
 int maxy, maxx;
 int x;
 char inp;
+
+/* init options */
 srand(time(NULL));
 srand48(time(NULL));
 initscr();
@@ -17,24 +20,35 @@ color_set(1,NULL);
 noecho();
 nodelay(stdscr,true);
 curs_set(0);
+
+/* declare matrix */
 getmaxyx(stdscr, maxy, maxx);
 unsigned int lines=(unsigned int)(drand48()*2*(maxx+maxy))+1;
 unsigned int delay=((unsigned int)(drand48()*20000)-(lines%20000)*2)+1000;
 signed long int matrix[lines][5];
+
+/* functions */
 unsigned char randchr(){
 return rand()%95+32;
 }
-for(x=0;x<lines;x++){
+void reinit(int y){
 matrix[x][1] = -((unsigned long int)rand()%maxy*2);
 matrix[x][2] = -((unsigned long int)rand()%maxy*2);
 if(matrix[x][2]>=matrix[x][1]) matrix[x][2]-=(unsigned int)(rand()%maxy);
-matrix[x][3] = (unsigned long int)rand()%maxx;
-matrix[x][5] = rand();
+matrix[x][3]=(unsigned int)rand()%maxx;
+matrix[x][5]=rand();
 }
+
+/* init matrix */
+for(x=0;x<lines;x++){
+reinit(x);
+}
+
+/* loop */
 while(1){
 getmaxyx(stdscr, maxy, maxx);
 inp=getch();
-if(inp=='q'||inp=='Q'){clear();refresh();endwin();exit(0);}
+if(inp=='q'||inp=='Q'){clear();refresh();endwin();exit(0);} /* quit */
 for(x=0;x<lines;x++){
 attron(COLOR_PAIR(1));
 if(matrix[x][1]>=0){
@@ -51,14 +65,10 @@ printw("%c",randchr());
 attron(COLOR_PAIR(2));
 if(matrix[x][1]>=0) mvprintw(matrix[x][1],matrix[x][3],"%c",matrix[x][4]);
 if(matrix[x][5]>rand()){
-matrix[x][2] = matrix[x][2]+1;
-if(matrix[x][2]>matrix[x][1]) matrix[x][2]-=(unsigned int)(rand()%maxy);
+matrix[x][2]++;
+if(matrix[x][2]>=matrix[x][1]) matrix[x][2]=matrix[x][1]-1;
 if(matrix[x][2]>maxy){
-matrix[x][1] = -((unsigned long int)rand()%maxy*2);
-matrix[x][2] = -((unsigned long int)rand()%maxy*2);
-if(matrix[x][2]>=matrix[x][1]) matrix[x][2]-=(unsigned int)(rand()%maxy);
-matrix[x][3]=(unsigned int)rand()%maxx;
-matrix[x][5]=rand();
+reinit(x);
 }
 }
 if(matrix[x][2]>=0) mvprintw(matrix[x][2],matrix[x][3]," ");
